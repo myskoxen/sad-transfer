20223afdae4585145dc161a5c9d86af475ff20dc
Robert Lilja <rlilja@feychting.se>
2021-10-22 09:01:54 +0200

    Started with some stubbs on some functions and a bit more on other functions

--- lib/SAD/Transfer.pm
+++ lib/SAD/Transfer.pm
@@ -3,6 +3,7 @@ package SAD::Transfer;
 use 5.006;
 use strict;
 use warnings;
+use Traverse::Dir;
 
 =head1 NAME
 
@@ -19,9 +20,10 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+Provides basic functions to setup a transfer function, including virus scanning,
+logging of meta data and more. Meant for easy configuration via a yaml file.
 
-Perhaps a little code snippet.
+Basically you run.
 
     use SAD::Transfer;
 
@@ -30,24 +32,122 @@ Perhaps a little code snippet.
 
 =head1 EXPORT
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+verify
 
 =head1 SUBROUTINES/METHODS
 
-=head2 function1
+=head2 check_for_new_files
+
+
 
 =cut
 
-sub function1 {
+sub check_for_new_files {
+    my $directory = shift;
+    opendir( my $dh, "$basepath/$dir") or die "Error: Unable to open $dir forreading: $!";
+    my @contents = grep(!/^.{1,2}$/, readdir($dh));
+    close $dh;
+    if( @contents ){
+        # spawn a separate process to handle the uploaded files
+        my $ret = spawn_watcher( $directory );
+        if( $ret ){
+            return 1;
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
 }
 
-=head2 function2
+=head2 spawn_watcher
 
 =cut
 
-sub function2 {
+sub spawn_watcher {
+    my $directory_name = shift;
+    my $blockfile = "$blockfiledir/$directory_name";
+    my $pid = fork;
+    if( defined $pid ){
+        # Fork worked
+        if( $pid ){
+            # Parent
+            return 1;
+        } else {
+            # Child
+            await_upload_finish( $directory_name );
+            run_transfer( $directory_name );
+            return 0;
+        }
+    }
 }
+
+=head2 run_transfer
+
+=cut
+
+sub run_transfer {
+    my $directory = shift;
+    wait_to_finish_upload( $directory, $upload_directory );
+    `mv $upload_path/$directory $working_path/$directory`;
+    virus_scan_contents( $directory, $working_path );
+    log_contents( $directory );
+    transfer_contents( $directory );
+}
+
+=head2 wait_to_finish_upload
+
+=cut
+
+sub wait_to_finish_upload {
+    my ($top_dir, $base_path) = @_;
+    my $former_size = `du -bs $base_path/$top_dir` or die "Failed to run system command du to get size $!";
+    my $timer = 6;
+    my $breakout = 1;
+    while $breakout {
+        sleep $timer;
+        # Check file size, if we grew files are still being uploaded,
+        # then we patiently wait some more :)
+        $current_size = `du -bs $base_path/$top_dir` or die "Failed to run system command du to get size $!";
+        if( $current_size == $former_size ){
+            # File is no longer being uploaded
+            $breakout = 0;
+        } else {
+            # Potentially recalculate sleep period (i.e. $timer)
+            if( $current_size >= $some_size ){
+                $timer = df;
+            }
+            $former_size = $current_size
+        }
+    }
+}
+
+=head2 virus_scan_contents
+
+=cut
+
+sub virus_scan_contents {
+    my ($top_dir, $base_path) = @_;
+    traverse_dir( "$base_path/$top_dir", \&virus_scanner );
+    
+}
+
+=head2 log_contents
+
+=cut
+
+sub log_contents {
+    my ($top_dir, $base_path) = @_;
+}
+
+=head2 transfer_contents
+
+=cut
+
+sub transfer_contents {
+    my ($top_dir, $base_path) = @_;
+}
+
 
 =head1 AUTHOR
 
